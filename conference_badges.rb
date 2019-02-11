344f81494626bb1d0f7ff57aa1179ec5c94a459f
Joshua Rivera-Sampayo <josh.rs1199@gmail.com>
2019-02-11 21:37:32 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -5,3 +5,12 @@ end
 def batch_badge_creator(name_list)
   name_list.collect {|name_list| badge_maker(name_list)}
 end
+
+def assign_rooms(attendees)
+  rooms = []
+  attendees.each_with_index do |attendees,index|
+    rooms.push("Hello, #{attendees}! You'll be assigned to room #{index + 1}!")
+end
+
+def printer
+end
